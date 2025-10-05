da59e11dc0697f79405117a89c7e99ba03f26908
KOTT830 <hosennaser830@gmail.com>
2025-10-05 08:33:51 +0300

    Update SW.js

--- mobile_app_files/SW.js
+++ mobile_app_files/SW.js
@@ -1,3 +1,25 @@
+const CACHE_NAME = "malazem-v1";
+const FILES_TO_CACHE = [
+  "/", 
+  "/index.html",
+  "/lecture_one_week_one.pdf",
+  "/leture2.pdf",
+  "/letur3.pdf",
+  "/Statistics_and_Probability_053030.pdf",
+  "/LEC1_DEF_COM_ARC.pdf",
+  "/LEC2_IAS_COMPUTER_component.pdf",
+  "/LEC3_COMPUTER_FUNCT.pdf",
+  "/LEC4_COM_MEM_SYS.pdf",
+  "/LEC5_INTERNAL_memory.pdf",
+  "/LEC6_AL_UNIT_CU.pdf",
+  "/LEC7_IO_Device.pdf",
+  "/HTML.pdf",
+  "/Logic_Design_Introduction.pdf",
+  "/Boolean_AlgebreLecture_2.pdf",
+  "/lecture1_103636.pdf",
+  "/Lecture1_Physics_Course.pdf"
+];
+
 self.addEventListener('install', (e) => {
   console.log('Service Worker: Installed');
 });
